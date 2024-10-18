aaeba86201584a9efdbd8db9be1892c20048fba5
ClaudioCumbene <ClaudioCumbene@gmail>
2024-10-18 18:17:10 +0200

    Order, Instant, ISO 8601- PART 2

--- src/main/java/com/claudiobeto/demo/config/TestConfig.java
+++ src/main/java/com/claudiobeto/demo/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.claudiobeto.demo.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.claudiobeto.demo.entities.Order;
 import com.claudiobeto.demo.entities.User;
+import com.claudiobeto.demo.repositories.OrderRepository;
 import com.claudiobeto.demo.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,9 @@ public class TestConfig  implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -24,6 +30,11 @@ public class TestConfig  implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
+		Order o1 = new Order(null, Instant.parse("2024-10-18T18:15:07Z"), u1); 
+		Order o2 = new Order(null, Instant.parse("2024-10-18T20:15:07Z"), u2); 
+		Order o3 = new Order(null, Instant.parse("2024-10-18T19:15:07Z"), u1);
+		
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 	
 	
