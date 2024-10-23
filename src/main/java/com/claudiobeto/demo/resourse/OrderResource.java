99dc9739dd2f752db0b9a9afc13e7b17d0f38981
ClaudioCumbene <ClaudioCumbene@gmail>
2024-10-23 13:02:10 +0200

    Product-OrderItem one-to-many association

--- src/main/java/com/claudiobeto/demo/resourse/OrderResource.java
+++ src/main/java/com/claudiobeto/demo/resourse/OrderResource.java
@@ -2,6 +2,7 @@ package com.claudiobeto.demo.resourse;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
